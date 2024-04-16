206a2c7acfa03581a5e3071c5c7d71e9b11f4770
kenichisutan <kenichi.jks@gmail.com>
2024-04-16 11:37:08 -0400

    Able to translate simple inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Translator translator = new Translator();
+        System.out.println(translator.translate("<human --> lifeform>."));
     }
 }
